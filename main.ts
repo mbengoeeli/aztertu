c61c7d5d0cefde7454a6a0f216b12e45ac09c8c3
mbengoeeli <132052481+mbengoeeli@users.noreply.github.com>
2023-05-02 14:58:03 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+function llamada_soinua1 () {
+	
+}
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        llamada_soinua1()
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        llamada_soinua1()
+    }
+})
